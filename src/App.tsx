9b0206581067f54f67f17ca3e1ee25982f5ce91f
astacus777 <astacus@hotmail.co.uk>
2024-10-26 00:55:33 +0100

    second commit- some folders added

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,38 @@
+import "./css/styles.css";
+import IMAGE from "./img/pic1.png";
+
 export const App = () => {
-    return <h1>Hello Yuou</h1>
-}
+  return (
+    <>
+      <h1 className="open">Hello Yuou</h1>
+      <svg width="400" height="180">
+        <rect
+          x="50"
+          y="20"
+          rx="20"
+          ry="20"
+          width="150"
+          height="150"
+          style={{
+            fill: "red",
+            stroke: "black",
+            strokeWidth: 5,
+            opacity: 0.5,
+          }}
+        />
+      </svg>
+      <svg width="300" height="200">
+        <polygon
+          points="100,10 40,198 190,78 10,78 160,198"
+          style={{
+            fill: "lime",
+            stroke: "purple",
+            strokeWidth: 5,
+            fillRule: "evenodd",
+          }}
+        />
+      </svg>
+      <img src={IMAGE} alt="clown" width="600" />
+    </>
+  );
+};
